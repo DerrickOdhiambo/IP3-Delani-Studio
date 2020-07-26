99f104ea9a4af4905b37d140cda375e384746ddb
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-07-26 18:16:46 +0300

    create a funtion using JavaScript to collect user input from the form in the webpage. It will also alert the user of submission of the form

--- js/script.js
+++ js/script.js
@@ -16,3 +16,23 @@ $(document).ready(function(){
     $("#fig-prod").slideToggle(1000);
   });
 });
+
+
+//Getting data from user
+function userMessage(){
+  document.getElementById("formSubmit").addEventListener("click", function(event){
+    event.preventDefault();
+    let userName = document.getElementById("name").value;
+    console.log(userName);
+    let userEmail = document.getElementById("email").value;
+    console.log(userEmail);
+    let userMessage = document.getElementById("message").value;
+    console.log(userMessage);
+  
+    if(userName && userEmail && userMessage){
+      alert(`Thank you for your feed back ${userName}.`);
+    }else{
+      alert("Please confirm your credentials.");
+    }
+  });
+}
