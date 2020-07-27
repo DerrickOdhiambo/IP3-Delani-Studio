e62a34786afef06bf7ff410ec4fb452931c395d1
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-07-27 07:31:22 +0300

    add the hover effect in the portfolio section, so that when the mouse hovers over it the name is shown

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//toggle effect in the what we do section
 $(document).ready(function(){
   $(".design").click(function(){
     $("#design-desc").toggle();
@@ -17,8 +18,59 @@ $(document).ready(function(){
   });
 });
 
+//Hover mouse effect in the portfolio section
+$(document).ready(function(){
+  $(".work-portfolio1").mouseenter(function(){
+    $("#overlay1").show();
+  });
+  $(".work-portfolio1").mouseleave(function(){
+    $("#overlay1").hide();
+  });
+  $(".work-portfolio2").mouseenter(function(){
+    $("#overlay2").show();
+  });
+  $(".work-portfolio2").mouseleave(function(){
+    $("#overlay2").hide();
+  });
+  $(".work-portfolio3").mouseenter(function(){
+    $("#overlay3").show();
+  });
+  $(".work-portfolio3").mouseleave(function(){
+    $("#overlay3").hide();
+  });
+  $(".work-portfolio4").mouseenter(function(){
+    $("#overlay4").show();
+  });
+  $(".work-portfolio4").mouseleave(function(){
+    $("#overlay4").hide();
+  });
+  $(".work-portfolio5").mouseenter(function(){
+    $("#overlay5").show();
+  });
+  $(".work-portfolio5").mouseleave(function(){
+    $("#overlay5").hide();
+  });
+  $(".work-portfolio6").mouseenter(function(){
+    $("#overlay6").show();
+  });
+  $(".work-portfolio6").mouseleave(function(){
+    $("#overlay6").hide();
+  });
+  $(".work-portfolio7").mouseenter(function(){
+    $("#overlay7").show();
+  });
+  $(".work-portfolio7").mouseleave(function(){
+    $("#overlay7").hide();
+  });
+  $(".work-portfolio8").mouseenter(function(){
+    $("#overlay8").show();
+  });
+  $(".work-portfolio8").mouseleave(function(){
+    $("#overlay8").hide();
+  });
+})
 
-//Getting data from user
+//Getting data from user through the form
 function userMessage(){
   document.getElementById("formSubmit").addEventListener("click", function(event){
     event.preventDefault();
